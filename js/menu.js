c1344e66517a9988504cbe8e9b2659227d558307
Nataliia <Natashashymanska@gmail.com>
2024-03-17 16:53:51 +0200

    add border radius 50 and remove second script

--- js/menu.js
+++ js/menu.js
@@ -39,10 +39,10 @@ console.log('Tets');
   openModalButton.addEventListener('click', openModal);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
+  // window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  //   if (!e.matches) return;
+  //   mobileMenu.classList.remove('is-open');
+  //   openMenuBtn.setAttribute('aria-expanded', false);
+  //   bodyScrollLock.enableBodyScroll(document.body);
+  // });
 })();
