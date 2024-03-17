2ff8b66d334a885acddfeadf03cc71cd1f2259aa
Nataliia <Natashashymanska@gmail.com>
2024-03-17 16:43:12 +0200

    add JS

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,12 @@
+console.log('Tets');
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  const closeMenuLink = document.querySelectorAll('.header-link');
+  const closeMenuLink = document.querySelectorAll('.mobile-nav-menu-link');
+  const modalContainer = document.querySelector('.close-modal');
+  const closeModalButton = document.querySelector('.modal-close-btn');
+  const openModalButton = document.querySelector('.order-service-btn');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -15,9 +19,24 @@
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
-  closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
+
+  const closeModal = () => {
+    modalContainer.classList.remove('is-open');
+  };
+
+  const openModal = () => {
+    modalContainer.classList.add('is-open');
+  };
+  closeMenuLink.forEach(item =>
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      toggleMenu();
+    })
+  );
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeModalButton.addEventListener('click', closeModal);
+  openModalButton.addEventListener('click', openModal);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
